2f9dd05164a1dcc5958349563411c9ca63a2dd1c
DaiYuXing <daiyu_zz@163.com>
2019-10-22 17:34:51 +0800

    自定义文件存储类

---
+++ utils/fdfs/storage.py
@@ -0,0 +1,29 @@
+from django.core.files.storage import Storage
+from fdfs_client.client import Fdfs_client
+
+
+class FDFSStorage(Storage):
+    '''fastdfs文件存储'''
+
+    def open(self, name, mode='rb'):
+        pass
+
+    def _save(self, name, content):
+        '''保存文件时使用'''
+        # name：你选择上传文件的名字
+        # content：包含你上传文件内容的File对象
+        # 创建一个Fdfs_client对象
+        client = Fdfs_client('.utils/fdfs/client.conf')
+
+        # 上传文件(content.read()文件内容),返回一个字典
+        res = client.upload_by_buffer(content.read())
+        if res.get('Status') != 'Upload successed':
+            # 上传失败
+            raise Exception('上传fastdfs失败')
+        # 获取返回的文件id
+        filename = res.get('Remote file_id')
+        return filename
+
+    def exists(self, name):
+        '''django判断文件名是否可用'''
+        return False
