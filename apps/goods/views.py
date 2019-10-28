649afa28230b22bc1ceef6864302d94537b27167
DaiYuXing <daiyu_zz@163.com>
2019-10-28 16:24:03 +0800

    用户历史浏览记录的添加

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -74,7 +74,7 @@ class DetailView(View):
         sku_orders = OrderGoods.objects.filter(sku=sku).exclude(commet='')
 
         # 获取新品信息
-        new_skus = GoodsSKU.objects.filter(type=sku.type).order_by('-create_time')
+        new_skus = GoodsSKU.objects.filter(type=sku.type).order_by('-create_time')[:2]
 
         # 获取用户购物车中商品的数目
         user = request.user
@@ -83,6 +83,17 @@ class DetailView(View):
             conn = get_redis_connection('default')
             cart_key = 'cart_%d' % user.id
             cart_count = conn.hlen(cart_key)
+
+            # 添加用户历史浏览记录
+            conn = get_redis_connection('default')
+            history_key = 'hostory_%d' % user.id
+            # 移除列表中的goods_id,
+            conn.lrem(history_key, 0, goods_id)
+            # 把goods_id插入列表中（左侧插入）
+            conn.lpush(history_key, goods_id)
+            # 只保存用户最新浏览的5条信息
+            conn.ltrim(history_key, 0, 4)
+
         else:
             # 用户未登录
             cart_count = 0
