d55a7d2560c82a8dd3d4eb5fcf496fb24e554bfa
DaiYuXing <daiyu_zz@163.com>
2019-10-31 15:19:32 +0800

    购物车记录前端js

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -25,17 +25,17 @@ class CartAddView(View):
         count = request.POST.get('count')
         # 数据校验
         if not all([sku_id, count]):
-            return JsonResponse({'res': 0, 'errmsg': '数据不完整'})
+            return JsonResponse({'res': 1, 'errmsg': '数据不完整'})
         # 校验添加的商品数量
         try:
             count = int(count)
         except Exception as e:
-            return JsonResponse({'res': 1, 'errmsg': '商品数目出错'})
+            return JsonResponse({'res': 2, 'errmsg': '商品数目出错'})
         # 校验商品是否存在
         try:
             sku = GoodsSKU.objects.get(id=sku_id)
         except GoodsSKU.DoesNotExist:
-            return JsonResponse({'res': 2, 'errmsg': '商品不存在'})
+            return JsonResponse({'res': 3, 'errmsg': '商品不存在'})
         # 业务处理：添加购物车记录
         conn = get_redis_connection('default')
         cart_key = 'cart_%d' % user.id
@@ -47,9 +47,11 @@ class CartAddView(View):
             count += int(cart_count)
         # 校验商品的库存
         if count > sku.stock:
-            return JsonResponse({'res': 3, 'errmsg': '商品库存不足'})
+            return JsonResponse({'res': 4, 'errmsg': '商品库存不足'})
         # 设置hash中sku_id对应的值
         # hset -> 如果sku_id已存在，更新数据，如果sku_id不存在，添加数据
         conn.hset(cart_key, sku_id, count)
+        # 计算用户购物车商品的条目数
+        total_count = conn.hlen(cart_key)
         # 返回应答
-        return JsonResponse({'res': 4, 'message': '添加成功'})
+        return JsonResponse({'res': 5, 'total_count': total_count, 'message': '添加成功'})
