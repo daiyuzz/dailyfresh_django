3b7ef682e32d8e3312a0e1115684f523689350b3
DaiYuXing <daiyu_zz@163.com>
2019-10-31 11:34:47 +0800

    购物车记录添加后台view

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from . import views
+from apps.cart.views import CartAddView
+
 app_name = 'user'
 
 urlpatterns = [
-
+    url(r'^add$', CartAddView.as_view(), name='add')  # 购物车记录添加
 ]
